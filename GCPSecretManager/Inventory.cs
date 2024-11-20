73fda1bcc01e1bf7c4f980b55e71ac0212838878
Lee Fine <lfine@keyfactor.com>
2024-11-20 15:00:22 +0000

    ab#17762

---
+++ GCPSecretManager/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Extensions;
+
+using Microsoft.Extensions.Logging;
+
+namespace Keyfactor.Extensions.Orchestrator.GCPSecretManager
+{
+    public class Inventory : IInventoryJobExtension
+    {
+        public string ExtensionName => "Keyfactor.Extensions.Orchestrator.GCPSecretManager.Inventory";
+
+        //Job Entry Point
+        public JobResult ProcessJob(InventoryJobConfiguration config, SubmitInventoryUpdate submitInventory)
+        {
+            ILogger logger = LogHandler.GetClassLogger(this.GetType());
+            logger.LogDebug($"Begin Inventory...");
+
+            List<CurrentInventoryItem> inventoryItems = new List<CurrentInventoryItem>();
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+            }
+
+            try
+            {
+                submitInventory.Invoke(inventoryItems);
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+            }
+            catch (Exception ex)
+            {
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+            }
+        }
+    }
+}
---
+++ GCPSecretManager/Management.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Keyfactor.Logging;
+using Keyfactor.Orchestrators.Extensions;
+using Keyfactor.Orchestrators.Common.Enums;
+
+using Microsoft.Extensions.Logging;
+
+namespace Keyfactor.Extensions.Orchestrator.SampleOrchestratorExtension
+{
+    public class Management : IManagementJobExtension
+    {
+        public string ExtensionName => "Keyfactor.Extensions.Orchestrator.GCPSecretManager.Management";
+
+        public JobResult ProcessJob(ManagementJobConfiguration config)
+        {
+            ILogger logger = LogHandler.GetClassLogger(this.GetType());
+            logger.LogDebug($"Begin Management...");
+
+            try
+            {
+                switch (config.OperationType)
+                {
+                    case CertStoreOperationType.Add:
+                        break;
+                    case CertStoreOperationType.Remove:
+                        break;
+                    case CertStoreOperationType.Create:
+                        break;
+                    default:
+                        return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = $"Site {config.CertificateStoreDetails.StorePath} on server {config.CertificateStoreDetails.ClientMachine}: Unsupported operation: {config.OperationType.ToString()}" };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = "Custom message you want to show to show up as the error message in Job History in KF Command" };
+            }
+
+            return new JobResult() { Result = Keyfactor.Orchestrators.Common.Enums.OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+        }
+    }
+}
